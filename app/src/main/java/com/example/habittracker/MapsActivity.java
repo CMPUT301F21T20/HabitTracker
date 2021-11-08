9814d314cbdbb8db415b2a643dc3a99a2b7744a0
lujiannan <jlu2@ualberta.ca>
2021-11-08 11:16:23 -0700

    get current location in google map

--- app/src/main/java/com/example/habittracker/MapsActivity.java
+++ app/src/main/java/com/example/habittracker/MapsActivity.java
@@ -1,12 +1,29 @@
 package com.example.habittracker;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Address;
+import android.location.Criteria;
 import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Looper;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -14,8 +31,10 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.habittracker.databinding.ActivityMapsBinding;
+import com.google.common.collect.Maps;
 
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.List;
 import java.util.Locale;
 
@@ -24,38 +43,49 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
     private String filterAddress = "";
+    private Location currentLocation;
+    private Button locationUpdateBtn;
+    private TextView updatedLocation_textView;
 
     // TODO: dynamically set the Latitude and Longitude
-    private double userLat = 1;
-    private double userLong = 1;
+    private double userLat;
+    private double userLong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        updatedLocation_textView = findViewById(R.id.updatedLocation_textView);
+        locationUpdateBtn = findViewById(R.id.locationUpdateBtn);
+        locationUpdateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getLocation();
+                setLocationTextView();
+            }
+        });
     }
 
     public void formatLocation() {
-        Geocoder geocoder = new Geocoder(getBaseContext(), Locale.getDefault());
+        Geocoder geocoder = new Geocoder(getBaseContext(), Locale.CANADA);
         try {
             List<Address> addresses =
-                    geocoder.getFromLocation(userLat, userLong, 1);
+                    geocoder.getFromLocation(currentLocation.getLatitude(), currentLocation.getLongitude(), 1);
 
             if (addresses.size() > 0) {
                 for (int i = 0; i < addresses.get(0).getMaxAddressLineIndex(); i++)
                     filterAddress += addresses.get(0).getAddressLine(i) + " ";
-                Toast.makeText(this,filterAddress,Toast.LENGTH_LONG).show();
             }
+            Toast.makeText(this, filterAddress, Toast.LENGTH_LONG).show();
         } catch (IOException ex) {
             ex.printStackTrace();
         } catch (Exception e2) {
-            // TODO: handle exception
             e2.printStackTrace();
         }
     }
@@ -73,10 +103,57 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        getLocation();
+        setLocationTextView();
+
         // Add a marker in Sydney and move the camera
-        LatLng latLng = new LatLng(0, 0);
-        mMap.addMarker(new MarkerOptions().position(latLng).title("default Location"));
+        LatLng latLng = new LatLng(userLat,userLong);
+        mMap.addMarker(new MarkerOptions().position(latLng).title("Current Location"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 5));
+    }
+
+    public void getLocation(){
+        LocationManager locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+        criteria.setHorizontalAccuracy(Criteria.ACCURACY_HIGH);
+        criteria.setAltitudeRequired(false);
+        criteria.setVerticalAccuracy(Criteria.ACCURACY_HIGH);
+        criteria.setSpeedRequired(false);
+        criteria.setCostAllowed(false);
+        criteria.setBearingRequired(false);
+        criteria.setPowerRequirement(Criteria.POWER_LOW);
+
+        // get the best available provider
+        String provider = locationManager.getBestProvider(criteria,true);
+        // get the first one from all the available ones
+        if (provider == null){
+            List<String> providers = locationManager.getProviders(true);
+            if (providers != null && providers.size() > 0) {
+                provider = providers.get(0);
+            }
+        }
+        // if no one is supported, return
+        if (TextUtils.isEmpty(provider)) {
+            return;
+        }
+
+        // check permission
+        if (ActivityCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        currentLocation = locationManager.getLastKnownLocation(provider);
+        userLat = currentLocation.getLatitude();
+        userLong = currentLocation.getLongitude();
+        Toast.makeText(this, "Latitude\t\t: " + (String.format("%+10.2f", userLat)) + "\n" +
+                "Longitude\t: " + (String.format("%+10.2f", userLong)),Toast.LENGTH_SHORT).show();
+    }
+
+    public void setLocationTextView(){
+        formatLocation();
+        updatedLocation_textView.setText(filterAddress);
     }
 }
