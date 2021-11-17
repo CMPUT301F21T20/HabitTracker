b1c80a511c8253f085d707c454c1e311801bd5d3
Vardan Saini <Vardan1@ualberta.ca>
2021-11-16 23:50:18 -0700

    Logout code added to fragmant from main

--- app/src/main/java/com/example/habittracker/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/habittracker/ui/profile/ProfileFragment.java
@@ -1,11 +1,13 @@
 package com.example.habittracker.ui.profile;
 
 import android.app.ActionBar;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -27,6 +29,7 @@ import com.google.firebase.auth.FirebaseAuth;
 public class ProfileFragment extends Fragment {
 
     private ProfileViewModel profileViewModel;
+    private Context thisContext;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -35,15 +38,24 @@ public class ProfileFragment extends Fragment {
         View root = inflater.inflate(R.layout.frag_profile, container, false);
         final ListView myHabits = root.findViewById(R.id.habitList);
         profileViewModel.getList().observe(getViewLifecycleOwner(), users -> {
-            // update UI
-        });
 
+        });
+        thisContext = container.getContext();
+        Button logoutButton = root.findViewById(R.id.logOutButton);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logout(view);
+            }
+        });
         return root;
+
     }
     public void logout(View view) {
         FirebaseAuth.getInstance().signOut();
-        startActivity(new Intent(getActivity().getApplicationContext(), LoginActivity.class));
+        startActivity(new Intent(thisContext, LoginActivity.class));
         getActivity().finish();
+
     }
 
 
