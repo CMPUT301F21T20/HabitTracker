b104162e2028b53157e758c8de2bd7e813a6cb48
Yash <ysrivast@ualberta.ca>
2021-11-18 00:47:48 -0700

    Shows recorded habits on habit event page

--- app/src/main/java/com/example/habittracker/classes/HabitEvent.java
+++ app/src/main/java/com/example/habittracker/classes/HabitEvent.java
@@ -1,25 +1,30 @@
 package com.example.habittracker.classes;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 public class HabitEvent implements Serializable {
+    private Habit habit;
     private String habitEventId;
     private String userId;
     private boolean isCompleted;
     private String imageUri;
     private String location;
     private String comment;
+    private Date date;
 
-    public HabitEvent(String habitEventId, String userId, boolean isCompleted,
-                      String imageUri, String location, String comment) {
+    public HabitEvent(Habit habit, String habitEventId, String userId, boolean isCompleted,
+                      String imageUri, String location, String comment, Date date) {
+        this.habit = habit;
         this.habitEventId = habitEventId;
         this.userId = userId;
         this.isCompleted = isCompleted;
         this.imageUri = imageUri;
         this.location = location;
         this.comment = comment;
+        this.date = date;
     }
 
     public HabitEvent() {}
@@ -30,7 +35,17 @@ public class HabitEvent implements Serializable {
         habitEvent.put("imageUri", this.imageUri);
         habitEvent.put("location", this.location);
         habitEvent.put("comment", this.comment);
+        habitEvent.put("date", this.date);
+        habitEvent.put("habitTitle", this.habit.getTitle());
         return habitEvent;
+    }
+
+    public Habit getHabit() {
+        return habit;
+    }
+
+    public void setHabit(Habit habit) {
+        this.habit = habit;
     }
 
     public String getHabitEventId() {
@@ -79,5 +94,13 @@ public class HabitEvent implements Serializable {
 
     public void setComment(String comment) {
         this.comment = comment;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
     }
 }
