c191880aa8117ace7a5f9435d1a13bc30489c79c
lujiannan <jlu2@ualberta.ca>
2021-11-18 00:22:58 -0700

    top tabLayout updated

---
+++ app/src/main/java/com/example/habittracker/HabitsUnbrokenFragment.java
@@ -0,0 +1,26 @@
+package com.example.habittracker;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class HabitsUnbrokenFragment extends Fragment {
+
+    public HabitsUnbrokenFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_habits_unbroken, container, false);
+    }
+}
--- app/src/main/java/com/example/habittracker/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/habittracker/ui/home/HomeFragment.java
@@ -1,65 +1,44 @@
 package com.example.habittracker.ui.home;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.viewpager2.widget.ViewPager2;
 
-import com.example.habittracker.AddNewHabitActivity;
 import com.example.habittracker.R;
-import com.example.habittracker.adapters.HabitListAdapter;
-import com.example.habittracker.classes.Habit;
-import com.example.habittracker.classes.HabitList;
-import com.example.habittracker.controllers.HabitListController;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-
-import java.util.Map;
+import com.example.habittracker.adapters.ViewPagerAdapter;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class HomeFragment extends Fragment {
     private HomeViewModel homeViewModel;
-    private FloatingActionButton addHabitButton;
-    private Context thisContext;
-    private HabitList habitList;
-    private ArrayAdapter<Habit> habitListAdapter;
-    private ListView habitsListView;
-    private FirebaseFirestore db;
+    private ViewPager2 habits_ViewPager2;
+    private TabLayout habits_TabLayout;
+    private String[] tabs = {"TOTAL", "TODAY", "UNBROKEN"};
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        thisContext = container.getContext();
-
-        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
-        db = FirebaseFirestore.getInstance();
-
         homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
-        addHabitButton = root.findViewById(R.id.addHabitButton);
-        habitsListView = root.findViewById(R.id.habits_listview);
+        habits_ViewPager2 = root.findViewById(R.id.habits_ViewPager2);
+        habits_TabLayout = root.findViewById(R.id.habits_TabLayout);
 
-        habitList = new HabitList();
-        habitListAdapter = new HabitListAdapter(thisContext, habitList);
-        habitsListView.setAdapter(habitListAdapter);
-
-        final CollectionReference collectionReference = db.collection("Habits");
+        habits_ViewPager2.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
+        habits_ViewPager2.setAdapter(new ViewPagerAdapter(this));
+        new TabLayoutMediator(habits_TabLayout, habits_ViewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                tab.setText(tabs[position]);
+            }
+        }).attach();
 
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
@@ -68,61 +47,6 @@ public class HomeFragment extends Fragment {
             }
         });
 
-        addHabitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openAddHabitActivity();
-            }
-        });
-
-        collectionReference.addSnapshotListener(new EventListener<QuerySnapshot>() {
-            @Override
-            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
-                handleCollectionUpdate(queryDocumentSnapshots, e);
-            }
-        });
-
         return root;
-    }
-
-    /**
-     * This function will handle opening the add new habit activity
-     */
-    public void openAddHabitActivity() {
-        Intent intent = new Intent(thisContext, AddNewHabitActivity.class);
-        startActivity(intent);
-    }
-
-    /**
-     * handles an update to the list of habits for a certain user
-     * @param queryDocumentSnapshots snapshots of the affected documents
-     * @param e an exception if raised
-     */
-    public void handleCollectionUpdate(QuerySnapshot queryDocumentSnapshots, FirebaseFirestoreException e) {
-        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
-        HabitList newHabitList = null;
-        HabitListController hc = new HabitListController();
-
-        for(QueryDocumentSnapshot doc: queryDocumentSnapshots)
-        {
-            // get the document that lists all habits for the user currently signed in
-            if (doc.getId().equals(uid)) {
-                Log.d("HANDLER", String.valueOf(doc.getData()));
-                Map<String, Object> docData = (Map<String, Object>) doc.getData();
-                newHabitList = hc.convertToHabitList(docData, uid);
-            }
-        }
-
-        // If the user has no habits
-        if (newHabitList == null) {
-            return;
-        }
-
-        habitList.clear();
-        for (int i = 0; i < newHabitList.getCount(); i++) {
-            Log.d("HANDLER", i + " -> " + newHabitList.get(i).getTitle());
-            habitList.addHabit(newHabitList.get(i));
-        }
-        habitListAdapter.notifyDataSetChanged();
     }
 }
