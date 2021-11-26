98d974fbd621cb6035fdfaafeb2f1f11274723a2
timolegros <timothee@ualberta.ca>
2021-11-26 10:35:12 -0700

    SocialController follower/following methods

--- app/src/main/java/com/example/habittracker/controllers/SocialController.java
+++ app/src/main/java/com/example/habittracker/controllers/SocialController.java
@@ -12,6 +12,7 @@ import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.SetOptions;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -74,17 +75,18 @@ public class SocialController {
         }
     }
 
+    /**
+     * This function saves an incoming or outgoing request. If the request is incoming and it was
+     * accepted then this function will trigger the follow() function which will also update
+     * follower/following lists in the users profile. When saving a request, the original request is
+     * saved in the originating users document and the inverted request is saved in the target users
+     * document. Inverted request just means flipping the type and the user ID (see flipType()).
+     * @param request The request to save
+     * @return A bool indicating failure or success
+     */
     public Boolean saveRequest(String type, Request request) {
         AtomicBoolean success = new AtomicBoolean(false);
         Map<String, Object> requestMap = request.getRequestMap();
-
-        // requestTypeMap = { [userId]: requestMap }
-//        Map<String, Map<String, Object>> requestTypeMap = new HashMap<>();
-//        requestTypeMap.put(request.getUserId(), requestMap);
-//
-//        // mapping = { "outgoing/incoming": requestTypeMap }
-//        Map<String, Map<String, Map<String, Object>>> mapping = new HashMap<>();
-//        mapping.put(type, requestTypeMap);
 
         // update the doc of the user who is submitting the change
         String path = type + "." + request.getUserId();
@@ -112,42 +114,107 @@ public class SocialController {
                 })
                 .addOnFailureListener(e -> Log.w("Firestore", "Error updating document", e));
 
+        // A status of 'Accepted' and type of 'incoming' means the user has accepted a follow request
+        if (request.getStatus().equals("Accepted") && type.equals("incoming")) this.follow(request);
+
         return success.get();
     }
 
-    public Boolean deleteRequest(String type, Request request) {
+    /**
+     * This function deletes a pending request. If the request is not pending it throws an error. A
+     * deleted incoming pending request automatically rejects the incoming request and marks the
+     * matching outgoing request as rejected. A deleted outgoing request simply deletes the outgoing
+     * request and the incoming request.
+     * @param type
+     * @param request
+     * @return
+     */
+    public Boolean deleteRequest(String type, Request request) throws Exception {
+        if (!request.getStatus().equals("Pending")) throw new Exception("Request must be pending");
         AtomicBoolean success = new AtomicBoolean(false);
 
-        // update the doc of the user who is submitting the change
-        String path = type + "." + request.getUserId();
-        Map<String, Object> updates = new HashMap<>();
-        updates.put(path, FieldValue.delete());
-        db.collection("Habits").document(this.user.getUid())
-                .update(updates)
-                .addOnSuccessListener(aVoid -> {
-                    success.set(true);
-                    Log.d("Firestore", "DocumentSnapshot successfully updated!");
-                })
-                .addOnFailureListener(e -> Log.w("Firestore", "Error updating document", e));
+        if (type.equals("incoming")) {
+            // delete incoming request
+            String path = "incoming." + request.getUserId();
+            Map<String, Object> updates = new HashMap<>();
+            updates.put(path, FieldValue.delete());
+            dbRequest("Requests", user.getUid(), updates);
 
+            // mark outgoing request as rejected
+            path = "outgoing." + user.getUid();
+            updates.clear();
+            Request clonedReq = request.cloneRequest();
+            clonedReq.setStatus("Rejected");
+            clonedReq.setUserId(user.getUid());
+            updates.put(path, clonedReq);
+            dbRequest("Requests", request.getUserId(), updates);
+        } else {
+            // delete outgoing request
+            String path = "outgoing." + request.getUserId();
+            Map<String, Object> updates = new HashMap<>();
+            updates.put(path, FieldValue.delete());
+            dbRequest("Requests", user.getUid(), updates);
 
-        path = flipType(type) + "." + this.user.getUid();
-        updates = new HashMap<>();
-        updates.put(path, FieldValue.delete());
-        db.collection("Habits").document(request.getUserId())
-                .update(updates)
-                .addOnSuccessListener(aVoid -> {
-                    success.set(true);
-                    Log.d("Firestore", "DocumentSnapshot successfully updated!");
-                })
-                .addOnFailureListener(e -> Log.w("Firestore", "Error updating document", e));
+            // delete incoming request
+            path = "incoming." + user.getUid();
+            updates.clear();
+            updates.put(path, FieldValue.delete());
+            dbRequest("Requests", request.getUserId(), updates);
+        }
 
         return success.get();
     }
+
+    /**
+     * This function adds a follower to the followers list of a user and a following to the following
+     * list of the target user. This function is triggered by the saveRequest() function when the
+     * type is 'incoming' and the status is 'Accepted'.
+     * @param request
+     */
+    private void follow(Request request) {
+        Date saveDate = new Date();
+
+        // update the current users followers in user profile
+        Map<String, Object> newFollower = new HashMap<>();
+        newFollower.put("followerSince", saveDate);
+        String path = "followers." + request.getUserId();
+        Map<String, Object> mapping = new HashMap<>();
+        mapping.put(path, newFollower);
+        dbRequest("Users", user.getUid(), mapping);
+
+        // update the target users (user who request to follow current user) following map
+        Map<String, Object> newFollowing = new HashMap<>();
+        newFollowing.put("followingSince", saveDate);
+        path = "following." + this.user.getUid();
+        mapping = new HashMap<>();
+        mapping.put(path, newFollowing);
+        dbRequest("users", request.getUserId(), mapping);
+
+    }
+
+    /**
+     * This function deletes a follower from the followers list and deletes a following from the
+     * following list. This function does not change or interact with requests in any way.
+     * @param userIdToUnfollow
+     * @return
+     */
+    public Boolean unfollow(String userIdToUnfollow) {
+        return true;
+    }
+
 
     private static String flipType(String type) {
         if (type.equals("incoming")) {
             return "outgoing";
         } else return "incoming";
+    }
+
+    private void dbRequest(String colId, String docId, Map<String, Object> data) {
+        db.collection(colId).document(docId)
+                .set(data, SetOptions.merge())
+                .addOnSuccessListener(aVoid -> {
+                    Log.d("Firestore", "DocumentSnapshot successfully updated!");
+                })
+                .addOnFailureListener(e -> Log.w("Firestore", "Error updating document", e));
     }
 }
