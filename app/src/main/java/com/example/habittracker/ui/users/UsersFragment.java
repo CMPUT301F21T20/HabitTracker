e01e40704b2a6df39cef9b0e7e466056c441ff4a
Vardan Saini <Vardan1@ualberta.ca>
2021-11-28 05:22:23 -0700

    Search functioning

--- app/src/main/java/com/example/habittracker/ui/users/UsersFragment.java
+++ app/src/main/java/com/example/habittracker/ui/users/UsersFragment.java
@@ -5,12 +5,14 @@ import static android.content.ContentValues.TAG;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -48,6 +50,8 @@ public class UsersFragment extends Fragment {
     private ArrayAdapter<User> userListAdapter;
     private FirebaseFirestore db;
     private Context thisContext;
+    EditText searchBar;
+    Button searchButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -66,6 +70,8 @@ public class UsersFragment extends Fragment {
 
         View root = inflater.inflate(R.layout.fragment_users, container, false);
 
+        searchBar = root.findViewById(R.id.searchBar);
+        searchButton = root.findViewById(R.id.searchButton);
         UsersListView = root.findViewById(R.id.users_listview);
 
         usersList = new UsersList();
@@ -91,6 +97,41 @@ public class UsersFragment extends Fragment {
                         }
                     }
                 });
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userName = searchBar.getText().toString().trim();
+                UsersList updatedUserList = new UsersList();
+
+                if(TextUtils.isEmpty(userName)){
+                    searchBar.setError("Username is required");
+                    return;
+                }
+                System.out.println(usersList.getCount());
+                for (int i =0; i < usersList.getCount(); i++) {
+                    System.out.println(i);
+                    System.out.println(usersList.getUser(i).getUsername());
+                    if (userName.equals(usersList.getUser(i).getUsername())) {
+                        //usersList.clearUserList();
+                        System.out.println("I am about to update updateUserLIst");
+                        updatedUserList.addUser(usersList.getUser(i));
+                        break;
+                    }
+
+                }
+
+                if (updatedUserList.getCount() > 0){
+                usersList.clearUserList();
+                usersList.addUser(updatedUserList.getUser(0));
+                userListAdapter.notifyDataSetChanged();
+                }
+                else {
+                    searchBar.setError("Incorrect username");
+                    return;
+                }
+            }
+
+        });
 
         return root;
 }
