a0f6993bb0da043cdeed3cdc2af7a624a225952f
spinningHorse <mark@outline.one>
2021-11-28 21:26:12 -0800

    Request Tab

--- app/src/main/java/com/example/habittracker/adapters/RequestAdapter.java
+++ app/src/main/java/com/example/habittracker/adapters/RequestAdapter.java
@@ -1,6 +1,8 @@
 package com.example.habittracker.adapters;
 
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,15 +14,17 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.example.habittracker.R;
+import com.example.habittracker.ViewHabitActivity;
 import com.example.habittracker.controllers.SocialController;
+import com.example.habittracker.models.Habit;
 import com.example.habittracker.models.Request;
 import com.example.habittracker.models.RequestMap;
 
-public class FollowingAdapter extends ArrayAdapter<Request> {
+public class RequestAdapter extends ArrayAdapter<Request> {
     private Context context;
     private RequestMap requestMap;
 
-    public FollowingAdapter(@NonNull Context context, RequestMap requestMap) throws Exception {
+    public RequestAdapter(@NonNull Context context, RequestMap requestMap) throws Exception {
         super(context, 0, requestMap.getRequestList("incoming"));
         this.context = context;
         this.requestMap = requestMap;
@@ -37,10 +41,12 @@ public class FollowingAdapter extends ArrayAdapter<Request> {
 
         try {
             Request request = requestMap.getRequest("incoming", position);
+            Log.d("TESTINGREQUESTESds", request.getUserId());
             TextView username = view.findViewById(R.id.username);
             username.setText(request.getUserName());
             Button acceptRequestButton = view.findViewById(R.id.acceptRequestButton);
             Button refuseRequestButton = view.findViewById(R.id.refuseRequestButton);
+            refuseRequestButton.setVisibility(View.VISIBLE);
             acceptRequestButton.setOnClickListener(v -> {
                 try {
                     request.setStatus(request.accepted);
@@ -49,12 +55,15 @@ public class FollowingAdapter extends ArrayAdapter<Request> {
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
+                acceptRequestButton.setText("Accepted");
+                refuseRequestButton.setVisibility(View.GONE);
             });
 
             refuseRequestButton.setOnClickListener(v -> {
                 try {
+                    request.setStatus(request.rejected);
                     SocialController controller = SocialController.getInstance();
-                    controller.deleteRequest("incoming", request);
+                    controller.saveRequest("incoming", request);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -64,10 +73,5 @@ public class FollowingAdapter extends ArrayAdapter<Request> {
         }
 
         return view;
-    }
-
-    @Override
-    public int getCount(){
-        return super.getCount();
     }
 }
